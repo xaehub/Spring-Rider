9edb5328a0b8546e9a117eef79572195da22936c
hyun2y00 <pottq577@naver.com>
2025-04-23 20:55:07 +0900

    feat: 프로젝트 초기 생성

---
+++ src/main/java/com/example/springrider/SpringRiderApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRiderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springrider/SpringRiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
