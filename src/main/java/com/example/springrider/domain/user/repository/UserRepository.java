ef315961be10256f2e41d5e4d25ae2d78f6f124c
hyun2y00 <pottq577@naver.com>
2025-04-24 19:14:04 +0900

    feat: 이메일이나 아이디로 유저 정보를 찾고 없으면 예외를 반환하는 default 메소드 생성

--- src/main/java/com/example/springrider/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/springrider/domain/user/repository/UserRepository.java
@@ -1,11 +1,24 @@
 package com.example.springrider.domain.user.repository;
 
+import com.example.springrider.domain.common.exception.ExceptionCode;
+import com.example.springrider.domain.common.exception.InvalidRequestException;
 import com.example.springrider.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
+
     boolean existsByEmail(String email); // 중복여부
+
     Optional<User> findByEmail(String email); // 이메일로 유저조회
+
+    default User findByEmailOrElseThrow(String email) {
+        return findByEmail(email)
+            .orElseThrow(() -> new InvalidRequestException(ExceptionCode.EMAIL_NOT_FOUND));
+    }
+
+    default User findByIdOrElseThrow(Long userId) {
+        return findById(userId)
+            .orElseThrow(() -> new InvalidRequestException(ExceptionCode.USER_NOT_FOUND));
+    }
 }
