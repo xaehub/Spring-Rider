051cb8e95856367c57e1fb01cc16b35223db06d3
김민준 <rlaalswns@naver.com>
2025-04-24 16:47:52 +0900

    feat: 회원탈퇴 기능 구현 및 응답 메시지 정리

---
+++ src/main/java/com/example/springrider/domain/user/dto/request/DeleteUserRequestDto.java
@@ -0,0 +1,18 @@
+package com.example.springrider.domain.user.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class DeleteUserRequestDto {
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private String password;
+
+
+}
