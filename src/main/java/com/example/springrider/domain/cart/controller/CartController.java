816e9d06a834771119191daa97a380933f9850a7
prem3dev <ecstasy1508@gmail.com>
2025-04-25 13:58:03 +0900

    feat: controller에 장바구니 조회 메서드 생성

--- src/main/java/com/example/springrider/domain/cart/controller/CartController.java
+++ src/main/java/com/example/springrider/domain/cart/controller/CartController.java
@@ -2,10 +2,12 @@ package com.example.springrider.domain.cart.controller;
 
 import com.example.springrider.domain.cart.dto.CartItemBulkRequestDto;
 import com.example.springrider.domain.cart.dto.CartItemBulkResponseDto;
+import com.example.springrider.domain.cart.dto.CartItemSearchBulkResponseDto;
 import com.example.springrider.domain.cart.service.CartService;
 import com.example.springrider.domain.common.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +30,12 @@ public class CartController {
         CartItemBulkResponseDto cartItemBulkResponseDto = cartService.addCartItems(userId,
             requestDto);
         return ApiResponse.ok(cartItemBulkResponseDto);
+    }
+
+    @GetMapping("/cart-items")
+    public ApiResponse<CartItemSearchBulkResponseDto> searchCartItems(
+        @SessionAttribute(name = "userId", required = false) Long userId) {
+
+        return ApiResponse.ok(cartService.searchCartItems(userId));
     }
 }
