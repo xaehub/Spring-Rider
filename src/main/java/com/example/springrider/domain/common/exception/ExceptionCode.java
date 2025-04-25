9c1b3a2da3a9ca318e9c5ef7e229a472c810ba61
prem3dev <ecstasy1508@gmail.com>
2025-04-25 12:06:15 +0900

    feat: 장바구니의 메뉴 중복요청 예외코드 생성

--- src/main/java/com/example/springrider/domain/common/exception/ExceptionCode.java
+++ src/main/java/com/example/springrider/domain/common/exception/ExceptionCode.java
@@ -38,6 +38,7 @@ public enum ExceptionCode {
     CART_EXCEPTION("장바구니 예외 메세지"),
     CART_STORE_MISMATCH("다른 가게의 메뉴를 추가하시려면 기존의 장바구니를 비워 주세요"),
     MENU_STORE_MISMATCH("해당 가게의 메뉴가 아닙니다."),
+    CART_DUPLICATE_ITEM("메뉴를 중복하지 마시고 수량을 변경해주세요."),
 
     // 리뷰
     REVIEW_EXCEPTION("리뷰 예외 메세지");
