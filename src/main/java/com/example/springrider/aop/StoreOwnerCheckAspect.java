f260a0212182d25baf1ad38e600722577c50051f
hyun2y00 <pottq577@naver.com>
2025-04-26 00:00:09 +0900

    feat: 가게의 주인인지 확인하는 AOP 클래스 생성

---
+++ src/main/java/com/example/springrider/aop/StoreOwnerCheckAspect.java
@@ -0,0 +1,50 @@
+package com.example.springrider.aop;
+
+import com.example.springrider.domain.common.exception.AuthException;
+import com.example.springrider.domain.common.exception.ExceptionCode;
+import com.example.springrider.domain.store.entity.Store;
+import com.example.springrider.domain.store.repository.StoreRepository;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class StoreOwnerCheckAspect {
+
+    private final StoreRepository storeRepository;
+
+    @Before("@annotation(storeOwnerCheck)")
+    public void checkStoreOwner(JoinPoint joinPoint, StoreOwnerCheck storeOwnerCheck) {
+        Object[] args = joinPoint.getArgs();
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        String[] paramNames = signature.getParameterNames();
+
+        Long userId = null;
+        Long storeId = null;
+
+        for (int i = 0; i < paramNames.length; i++) {
+            if (paramNames[i].equals(storeOwnerCheck.userIdParam())) {
+                userId = (Long) args[i];
+            }
+            if (paramNames[i].equals(storeOwnerCheck.storeIdParam())) {
+                storeId = (Long) args[i];
+            }
+        }
+
+        if (userId == null || storeId == null) {
+            throw new IllegalArgumentException("userId 나 storeId를 찾을 수 없습니다.");
+        }
+
+        Store store = storeRepository.findByIdOrElseThrow(storeId);
+        if (!Objects.equals(store.getUser().getId(), userId)) {
+            throw new AuthException(ExceptionCode.STORE_ACCESS_DENIED);
+        }
+    }
+
+}
