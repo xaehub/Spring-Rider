ecd9cc9088d479087a5f0012e5f5aaddf2e09114
xaehub <novxae@gmail.com>
2025-04-25 20:06:09 +0900

    refactoring: 메서드명 수정

--- src/main/java/com/example/springrider/domain/store/entity/Store.java
+++ src/main/java/com/example/springrider/domain/store/entity/Store.java
@@ -6,7 +6,7 @@ import com.example.springrider.domain.common.entity.BaseEntity;
 import com.example.springrider.domain.common.exception.InvalidRequestException;
 import com.example.springrider.domain.menu.entity.Menu;
 import com.example.springrider.domain.store.dto.StoreRequestDto;
-import com.example.springrider.domain.store.dto.StoreUpdateRequestDto;
+import com.example.springrider.domain.store.dto.UpdateStoreRequestDto;
 import com.example.springrider.domain.store.enums.StoreStatus;
 import com.example.springrider.domain.user.entity.User;
 import jakarta.persistence.CascadeType;
@@ -25,9 +25,11 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @Table(name = "store")
 @AllArgsConstructor
 @NoArgsConstructor
@@ -86,7 +88,7 @@ public class Store extends BaseEntity {
         );
     }
 
-    public void update(StoreUpdateRequestDto dto) {
+    public void update(UpdateStoreRequestDto dto) {
         this.name = dto.getName();
         this.address = dto.getAddress();
         this.category = dto.getCategory();
