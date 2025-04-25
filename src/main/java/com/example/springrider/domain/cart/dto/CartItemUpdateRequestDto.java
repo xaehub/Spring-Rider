379a5a2da276bdc581fd2e11adfa5240e267f747
prem3dev <ecstasy1508@gmail.com>
2025-04-25 19:24:04 +0900

    feat: CartItemUpdateRequestDto 생성
    - 수정 요청의 수량 필드의 null, 수량이 0보다 큰지 여부를 판단하는 메서드가 있다.
    
    - 상태 필드의 null 여부를 판단하는 메서드가 있다.

---
+++ src/main/java/com/example/springrider/domain/cart/dto/CartItemUpdateRequestDto.java
@@ -0,0 +1,23 @@
+package com.example.springrider.domain.cart.dto;
+
+import com.example.springrider.domain.common.customannotation.NotAllNull;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@NotAllNull
+@RequiredArgsConstructor
+public class CartItemUpdateRequestDto {
+
+    private final String status;
+
+    private final Integer quantity;
+
+    public boolean isValidQuantity() {
+        return quantity != null && quantity > 0;
+    }
+
+    public boolean hasStatus() {
+        return status != null;
+    }
+}
