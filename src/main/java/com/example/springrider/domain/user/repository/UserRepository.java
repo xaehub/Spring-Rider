f327f0bf28efdff2fb30724e1dd4e8436123b2aa
prem3dev <ecstasy1508@gmail.com>
2025-04-25 09:42:51 +0900

    style: 단순 정렬

--- src/main/java/com/example/springrider/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/springrider/domain/user/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.example.springrider.domain.user.repository;
 
 import com.example.springrider.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
+
     boolean existsByEmail(String email); // 중복여부
+
     Optional<User> findByEmail(String email); // 이메일로 유저조회
 }
