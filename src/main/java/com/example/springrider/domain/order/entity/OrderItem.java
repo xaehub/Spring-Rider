661ac46f00de4f046890ccf7f40dbed25fcebb91
prem3dev <ecstasy1508@gmail.com>
2025-04-25 16:03:36 +0900

    feat: OrderItem Entity 구현
    주문, 메뉴, 수량 필드를 가지고 있다.

---
+++ src/main/java/com/example/springrider/domain/order/entity/OrderItem.java
@@ -0,0 +1,30 @@
+package com.example.springrider.domain.order.entity;
+
+import com.example.springrider.domain.common.entity.BaseEntity;
+import com.example.springrider.domain.menu.entity.Menu;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+public class OrderItem extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "menu_id", nullable = false)
+    private Menu menu;
+
+    @Column(nullable = false)
+    private Integer quantity;
+}
