959ad55f3ea0c681f5a5c878951df06af0937979
prem3dev <ecstasy1508@gmail.com>
2025-04-26 16:46:02 +0900

    refactor: 리스트 제네릭 내부 dto 명칭 수정

--- src/main/java/com/example/springrider/domain/cart/dto/CreateCartItemBulkRequestDto.java
+++ src/main/java/com/example/springrider/domain/cart/dto/CreateCartItemBulkRequestDto.java
@@ -8,11 +8,11 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class CreationCartItemBulkRequestDto {
+public class CreateCartItemBulkRequestDto {
 
     @NotNull(message = "가게Id는 필수 값입니다.")
     private final Long storeId;
 
     @NotEmpty(message = "1개 이상의 메뉴를 요청해야 합니다.")
-    private final List<CreationCartItemRequestDto> cartItems;
+    private final List<CreateCartItemRequestDto> cartItems;
 }
