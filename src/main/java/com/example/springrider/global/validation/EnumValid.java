7db6db1248acdb854cc71dc2678172dbe2916c98
prem3dev <ecstasy1508@gmail.com>
2025-04-27 02:59:49 +0900

    feat: EnumValid annotation 생성
    런타임 중 적용되며, 필드를 대상으로 enumClass에 대한 유효성 검증을 수행한다.

---
+++ src/main/java/com/example/springrider/global/validation/EnumValid.java
@@ -0,0 +1,24 @@
+package com.example.springrider.global.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Constraint(validatedBy = EnumValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EnumValid {
+
+    Class<? extends Enum<?>> enumClass();
+
+    String message() default "올바르지 않은 값입니다.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
