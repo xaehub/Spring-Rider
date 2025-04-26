3874638c25f38142705ecab3783de047a9112401
prem3dev <ecstasy1508@gmail.com>
2025-04-26 20:56:54 +0900

    feat: 장바구니 예외코드 수정
    - EMPTY_CART로 변경
    
    - CARTITEM_NOT_FOUND로 변경
    
    - CARTITEM_DELETE_FAIL 추가

--- src/main/java/com/example/springrider/domain/common/exception/ExceptionCode.java
+++ src/main/java/com/example/springrider/domain/common/exception/ExceptionCode.java
@@ -43,9 +43,10 @@ public enum ExceptionCode {
     CART_STORE_MISMATCH("다른 가게의 메뉴를 추가하시려면 기존의 장바구니를 비워 주세요"),
     MENU_STORE_MISMATCH("해당 가게의 메뉴가 아닙니다."),
     CART_DUPLICATE_ITEM("메뉴를 중복하지 마시고 수량을 변경해주세요."),
-    CART_NOT_FOUND_ALL("장바구니가 존재하지 않습니다."),
-    CART_NOT_FOUND("해당 장바구니 품목이 존재하지 않습니다."),
+    EMPTY_CART("장바구니가 존재하지 않습니다."),
+    CARTITEM_NOT_FOUND("해당 장바구니 품목이 존재하지 않습니다."),
     INVALID_CARTITEM_STATUS("올바른 장바구니 상태 코드가 아닙니다."),
+    CARTITEM_DELETE_FAIL("장바구니 항목 삭제에 실패했습니다."),
 
     // 리뷰
     REVIEW_EXCEPTION("리뷰 예외 메세지"),
