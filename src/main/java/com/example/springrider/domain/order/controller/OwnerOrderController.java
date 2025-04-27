c04b1231926b2f4a0a14cc8a915acc884a2ab00e
prem3dev <ecstasy1508@gmail.com>
2025-04-27 19:43:07 +0900

    feat: OwnerOrderController에 delete 메서드 추가

--- src/main/java/com/example/springrider/domain/order/controller/OwnerOrderController.java
+++ src/main/java/com/example/springrider/domain/order/controller/OwnerOrderController.java
@@ -1,16 +1,20 @@
 package com.example.springrider.domain.order.controller;
 
 import com.example.springrider.domain.common.response.ApiResponse;
-import com.example.springrider.domain.order.dto.UpdateOrderStatusRequestDto;
-import com.example.springrider.domain.order.dto.UpdateOrderStatusResponseDto;
+import com.example.springrider.domain.order.dto.request.CancelOrderRequestDto;
+import com.example.springrider.domain.order.dto.request.UpdateOrderStatusRequestDto;
+import com.example.springrider.domain.order.dto.response.CancelOrderResponseDto;
+import com.example.springrider.domain.order.dto.response.UpdateOrderStatusResponseDto;
 import com.example.springrider.domain.order.service.OwnerOrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @RestController
 @RequestMapping("/api/owners/stores")
@@ -25,5 +29,14 @@ public class OwnerOrderController {
         @Valid @RequestBody UpdateOrderStatusRequestDto requestDto
     ) {
         return ApiResponse.ok(ownerOrderService.update(orderId, requestDto));
+    }
+
+    @DeleteMapping("/{storeId}/orders/{orderId}")
+    public ApiResponse<CancelOrderResponseDto> delete(
+        @PathVariable Long storeId, @PathVariable Long orderId,
+        @Valid @RequestBody CancelOrderRequestDto requestDto,
+        @SessionAttribute Long userId
+    ) {
+        return ApiResponse.ok(ownerOrderService.delete(storeId, orderId, userId, requestDto));
     }
 }
