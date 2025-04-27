bf2867dc38b96c1df484720cd06735ae3bd59d18
prem3dev <ecstasy1508@gmail.com>
2025-04-27 19:46:00 +0900

    feat: valid annotation 메세지 추가

--- src/main/java/com/example/springrider/domain/order/dto/request/UpdateOrderStatusRequestDto.java
+++ src/main/java/com/example/springrider/domain/order/dto/request/UpdateOrderStatusRequestDto.java
@@ -1,4 +1,4 @@
-package com.example.springrider.domain.order.dto;
+package com.example.springrider.domain.order.dto.request;
 
 import com.example.springrider.domain.order.enums.OrderStatus;
 import com.example.springrider.global.validation.EnumValid;
