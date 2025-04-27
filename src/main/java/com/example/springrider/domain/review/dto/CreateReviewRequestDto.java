0ab8ab11e047c16337ddbb1f408cdc3d03b56315
prem3dev <ecstasy1508@gmail.com>
2025-04-27 23:26:39 +0900

    feat: CreateReviewRequestDto 생성

---
+++ src/main/java/com/example/springrider/domain/review/dto/CreateReviewRequestDto.java
@@ -0,0 +1,20 @@
+package com.example.springrider.domain.review.dto;
+
+import com.example.springrider.domain.review.enums.Rating;
+import com.example.springrider.global.validation.EnumValid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class CreateReviewRequestDto {
+
+    @Size(max = 50, message = "리뷰 최대 글자 수는 50자 입니다.")
+    private final String contents;
+
+    @NotNull
+    @EnumValid(enumClass = Rating.class, message = "올바르지 않은 등급 코드입니다.")
+    private final String rating;
+}
