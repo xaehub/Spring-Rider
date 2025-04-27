da056c927647129f28297fb897d14aa5a4c1ce37
hyun2y00 <pottq577@naver.com>
2025-04-27 23:47:44 +0900

    rename: ResponseInterceptor 클래스명 변경 (ResponseInterceptor -> ApiResponseAdvice)

--- src/main/java/com/example/springrider/global/response/ApiResponseAdvice.java
+++ src/main/java/com/example/springrider/global/response/ApiResponseAdvice.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 @RestControllerAdvice
-public class ResponseInterceptor implements ResponseBodyAdvice {
+public class ApiResponseAdvice implements ResponseBodyAdvice {
 
     @Override
     public boolean supports(MethodParameter returnType, Class converterType) {
