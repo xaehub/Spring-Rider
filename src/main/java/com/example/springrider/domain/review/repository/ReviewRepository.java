7946e548e8d327ef02c943cf7a23e4ad7bb03c66
prem3dev <ecstasy1508@gmail.com>
2025-04-28 13:50:10 +0900

    feat: storeId로 리뷰를 전체 조회하는 jpa 쿼리 메서드 추가
    페이징 여부 고려

--- src/main/java/com/example/springrider/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/springrider/domain/review/repository/ReviewRepository.java
@@ -1,9 +1,21 @@
 package com.example.springrider.domain.review.repository;
 
 import com.example.springrider.domain.review.entity.Review;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     boolean existsByOrderId(Long orderId);
+
+    @Query("""
+        SELECT DISTINCT r FROM Review r
+        JOIN FETCH r.order o
+        JOIN FETCH o.store s
+        WHERE s.id = :storeId
+        """)
+    List<Review> findAllByStoreId(
+        @Param("storeId") Long storeId);
 }
