d37afafa7748c8684589709ebe5e60fb6bc98f83
prem3dev <ecstasy1508@gmail.com>
2025-04-28 15:08:44 +0900

    feat: 조회 방법을 페이징 처리로 수정
    page 번호를 path variable로 받는다.
    = 1부터 페이지 숫자를 받아 사용자 편의를 주기 위함

--- src/main/java/com/example/springrider/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/springrider/domain/review/controller/ReviewController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
@@ -30,9 +31,12 @@ public class ReviewController {
         return ApiResponse.ok(reviewService.create(orderId, userId, requestDto));
     }
 
-    @GetMapping("/customers/stores/{storeId}/reviews")
+    @GetMapping("/customers/stores/{storeId}/reviews/page/{pageNumber}")
     public ApiResponse<FindAllReviewResponseDto> findAll(
-        @PathVariable Long storeId) {
-        return ApiResponse.ok(reviewService.findAll(storeId));
+        @PathVariable Long storeId,
+        @PathVariable int pageNumber,
+        @RequestParam(defaultValue = "10") int size
+    ) {
+        return ApiResponse.ok(reviewService.findAll(storeId, pageNumber, size));
     }
 }
