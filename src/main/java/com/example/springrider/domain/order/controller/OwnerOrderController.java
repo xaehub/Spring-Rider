8aabf1d50dcf079c21ce349ec2bb6f5bad8734eb
hyun2y00 <pottq577@naver.com>
2025-04-28 12:00:38 +0900

    refactor(order): userId 상수로 사용

--- src/main/java/com/example/springrider/domain/order/controller/OwnerOrderController.java
+++ src/main/java/com/example/springrider/domain/order/controller/OwnerOrderController.java
@@ -1,17 +1,18 @@
 package com.example.springrider.domain.order.controller;
 
-import com.example.springrider.global.response.ApiResponse;
+import com.example.springrider.config.Const;
 import com.example.springrider.domain.order.dto.request.CancelOrderRequestDto;
 import com.example.springrider.domain.order.dto.request.UpdateOrderStatusRequestDto;
 import com.example.springrider.domain.order.dto.response.CancelOrderResponseDto;
-import com.example.springrider.domain.order.dto.response.UpdateOrderStatusResponseDto;
 import com.example.springrider.domain.order.dto.response.OwnerOrderResponseDto;
+import com.example.springrider.domain.order.dto.response.UpdateOrderStatusResponseDto;
 import com.example.springrider.domain.order.service.OwnerOrderService;
+import com.example.springrider.global.response.ApiResponse;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,7 +31,7 @@ public class OwnerOrderController {
     public ApiResponse<UpdateOrderStatusResponseDto> update(
         @PathVariable Long storeId, @PathVariable Long orderId,
         @Valid @RequestBody UpdateOrderStatusRequestDto requestDto,
-        @SessionAttribute(name = "userId") Long userId
+        @SessionAttribute(name = Const.SESSION_USER_ID) Long userId
     ) {
         return ApiResponse.ok(ownerOrderService.update(orderId, storeId, userId, requestDto));
     }
@@ -38,7 +39,7 @@ public class OwnerOrderController {
     @GetMapping("/{storeId}/orders")
     public ApiResponse<List<OwnerOrderResponseDto>> findAll(
         @PathVariable Long storeId,
-        @SessionAttribute(name = "userId") Long userId
+        @SessionAttribute(name = Const.SESSION_USER_ID) Long userId
     ) {
         return ApiResponse.ok(ownerOrderService.findAll(storeId, userId));
     }
@@ -47,7 +48,7 @@ public class OwnerOrderController {
     public ApiResponse<CancelOrderResponseDto> delete(
         @PathVariable Long storeId, @PathVariable Long orderId,
         @Valid @RequestBody CancelOrderRequestDto requestDto,
-        @SessionAttribute Long userId
+        @SessionAttribute(name = Const.SESSION_USER_ID) Long userId
     ) {
         return ApiResponse.ok(ownerOrderService.delete(storeId, orderId, userId, requestDto));
     }
