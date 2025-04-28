f182a76417dfa22db36b0bd0d1b189627bc02c34
prem3dev <ecstasy1508@gmail.com>
2025-04-28 22:18:54 +0900

    feat: @NotBlank 추가
    status 필드가 String으로 받기 때문에 @NotAllNull 어노테이션 제거하고 status 필드 위에 @NotBlank 추가

--- src/main/java/com/example/springrider/domain/cart/dto/request/UpdateCartItemRequestDto.java
+++ src/main/java/com/example/springrider/domain/cart/dto/request/UpdateCartItemRequestDto.java
@@ -2,17 +2,19 @@ package com.example.springrider.domain.cart.dto.request;
 
 import com.example.springrider.domain.cart.enums.CartItemStatus;
 import com.example.springrider.global.validation.EnumValid;
-import com.example.springrider.global.validation.NotAllNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@NotAllNull
 @RequiredArgsConstructor
 public class UpdateCartItemRequestDto {
 
     @EnumValid(enumClass = CartItemStatus.class, message = "올바르지 않은 장바구니 상태 코드입니다.")
+    @NotBlank
     private final String status;
 
+    @NotNull
     private final Integer quantity;
 }
