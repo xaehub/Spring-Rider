711d94155d6419c853aa576e3e60e544aa4fc499
prem3dev <ecstasy1508@gmail.com>
2025-04-28 13:48:17 +0900

    feat: controller에 리뷰 조회 메서드 추가
    url 분리 필요에 따른 수정이 포함되어있다.

--- src/main/java/com/example/springrider/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/springrider/domain/review/controller/ReviewController.java
@@ -2,11 +2,13 @@ package com.example.springrider.domain.review.controller;
 
 
 import com.example.springrider.domain.review.dto.request.CreateReviewRequestDto;
+import com.example.springrider.domain.review.dto.response.FindAllReviewResponseDto;
 import com.example.springrider.domain.review.dto.response.ReviewResponseDto;
 import com.example.springrider.domain.review.service.ReviewService;
 import com.example.springrider.global.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,16 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 @RestController
-@RequestMapping("/api/customers/orders")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class ReviewController {
 
     private final ReviewService reviewService;
 
-    @PostMapping("/{orderId}/reviews")
+    @PostMapping("/customers/orders/{orderId}/reviews")
     public ApiResponse<ReviewResponseDto> create(
         @PathVariable Long orderId, @Valid @RequestBody CreateReviewRequestDto requestDto,
         @SessionAttribute(name = "userId") Long userId) {
         return ApiResponse.ok(reviewService.create(orderId, userId, requestDto));
+    }
+
+    @GetMapping("/customers/stores/{storeId}/reviews")
+    public ApiResponse<FindAllReviewResponseDto> findAll(
+        @PathVariable Long storeId) {
+        return ApiResponse.ok(reviewService.findAll(storeId));
     }
 }
