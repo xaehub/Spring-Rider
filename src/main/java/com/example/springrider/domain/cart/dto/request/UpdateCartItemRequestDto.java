a67af64696e31de3965b713bc1deaa34ebc5bf8f
prem3dev <ecstasy1508@gmail.com>
2025-04-29 09:14:01 +0900

    feat: @NotAllNull 롤백
    @EnumValid가 공백의 경우도 false를 반환해주고 공백은 올바르지 않은 enum 상수라는 메세지도 이질감이 들지 않기 때문에 롤백합니다.

--- src/main/java/com/example/springrider/domain/cart/dto/request/UpdateCartItemRequestDto.java
+++ src/main/java/com/example/springrider/domain/cart/dto/request/UpdateCartItemRequestDto.java
@@ -2,17 +2,17 @@ package com.example.springrider.domain.cart.dto.request;
 
 import com.example.springrider.domain.cart.enums.CartItemStatus;
 import com.example.springrider.global.validation.EnumValid;
-import jakarta.validation.constraints.NotBlank;
+import com.example.springrider.global.validation.NotAllNull;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@NotAllNull
 public class UpdateCartItemRequestDto {
 
     @EnumValid(enumClass = CartItemStatus.class, message = "올바르지 않은 장바구니 상태 코드입니다.")
-    @NotBlank
     private final String status;
 
     @NotNull
